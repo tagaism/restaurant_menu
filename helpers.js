6804fa4df695fa311e903576ac3f46b58f5dc784
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2022-01-16 22:56:36 -0600

    Rendering data dynamically for each dish into main menu

--- helpers.js
+++ helpers.js
@@ -1,8 +1,39 @@
 // fetch data
 const API_URL = "https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json";
 
-export const getData = async () => {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    return data;
-}
+// export const getData = async () => {
+//     const response = await fetch(API_URL);
+//     const data = await response.json();
+//     return data;
+// }
+
+// export const getData = fetch(API_URL)
+//     .then(response => response.json())
+//     .then(data => data)
+
+export const data = [
+    {
+    "id": 1,
+    "title": "buttermilk pancakes",
+    "category": "breakfast",
+    "price": 15.99,
+    "img": "./images/item-1.jpeg",
+    "desc": "`I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `"
+    },
+    {
+    "id": 2,
+    "title": "diner double",
+    "category": "lunch",
+    "price": 13.99,
+    "img": "./images/item-2.jpeg",
+    "desc": "`vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `"
+    },
+    {
+    "id": 3,
+    "title": "godzilla milkshake",
+    "category": "shakes",
+    "price": 6.99,
+    "img": "./images/item-3.jpeg",
+    "desc": "`ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`"
+    }
+]
--- main.js
+++ main.js
@@ -1,3 +1,48 @@
-import { getData } from "./helpers.js";
+import { data, getData } from "./helpers.js";
 
-const data = getData();
+const data = getData;
+
+const dishImage = document.querySelector("img")
+const dishName = document.querySelector(".dish-name")
+const dishPrice = document.querySelector(".dish-price")
+const dishInfo = document.querySelector(".dish-info")
+const mainMenu = document.querySelector("main")
+
+console.log(data)
+
+// method for rendering dishes into main menu
+const renderDataToMenu = (data) => {
+
+    // can be refactored by using object destructuring
+    // const [title, price, img, desc] = data;
+
+    // looping over the objects of array and rendering dishes
+    data.forEach(dish => {
+
+        // creating a div element for each dish into main container
+        let cardDiv = document.createElement("div");
+        // appending each div to main container
+        mainMenu.appendChild(cardDiv);
+        // adding the class name for div
+        cardDiv.className = "card flex-with-direction";
+        // rendering the data into each div
+        cardDiv.innerHTML =
+            `<div class="img">
+                    <img src="${dish.img}
+                    " alt="${dish.title}">
+                </div>
+                <div class="description">
+                    <div class="name-and-price flex-with-direction white-opacity">
+                        <h3 class="dish-name">${dish.title}</h3>
+                        <h3 class="dish-price">${dish.price}</h3>
+                    </div>
+                    <div class="dish-info">
+                        <p>${dish.desc}</p>
+                    </div>
+                </div>
+            `
+    })
+
+}
+
+renderDataToMenu(data)
