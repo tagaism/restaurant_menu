dc5918ff0facffc9809c9792fbd28207c6b8c5c4
tagaism <i77tag@gmail.com>
2022-01-18 12:08:26 +0900

    fix fetching API data from the source.

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-import {
-    data,
-    getData
-} from "./helpers.js";
-
-const data = getData;
+// fetch data
+const API_URL = "https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json";
 
 const dishImage = document.querySelector("img");
 const dishName = document.querySelector(".dish-name");
@@ -12,26 +8,19 @@ const dishInfo = document.querySelector(".dish-info");
 const mainMenu = document.querySelector("main");
 
 
-// method for rendering dishes into main menu
-const renderDataToMenu = (data) => {
-
-    // can be refactored by using object destructuring
-    // const [title, price, img, desc] = data;
-
-    // looping over the objects of array and rendering dishes
-    data.forEach(dish => {
-
-        // creating a div element for each dish into main container
-        let cardDiv = document.createElement("div");
-        // appending each div to main container
-        mainMenu.appendChild(cardDiv);
-        // adding the class name for div
-        cardDiv.className = "card flex-with-direction";
-        // rendering the data into each div
-        cardDiv.innerHTML =
-            `<div class="img">
-                    <img src="${dish.img}
-                    " alt="${dish.title}">
+const renderDataToMenu = () => {
+    fetch(API_URL).then(resp => resp.json()).then(data => {
+        data.forEach(dish => {
+            // creating a div element for each dish into main container
+            let cardDiv = document.createElement("div");
+            // appending each div to main container
+            mainMenu.appendChild(cardDiv);
+            // adding the class name for div
+            cardDiv.className = "card flex-with-direction";
+            // rendering the data into each div
+            cardDiv.innerHTML =`
+                <div class="img">
+                    <img src="${dish.img}" alt="${dish.title}">
                 </div>
                 <div class="description">
                     <div class="name-and-price flex-with-direction white-opacity">
@@ -43,7 +32,7 @@ const renderDataToMenu = (data) => {
                     </div>
                 </div>
             `
+        })
     })
 }
-
-renderDataToMenu(data);
+renderDataToMenu();
